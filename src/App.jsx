87bbf716fc4a26b5cd177f2da47f9c728cca15da
cdvg1992 <vangroeningen.c@outlook.com>
2022-05-16 15:56:19 +0200

    add side

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import Slide from './components/Slide';
 import './index.css';
+import { Routes, Route, Link } from "react-router-dom";
+import About from './pages/About'
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <div className="App">
 
-      <Slide/>
-   
+      <div className='Left_side'>test2</div>
+      <div classnae='right_side'></div>
+      
+
+      <Routes>
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+
+      {/* <Slide/> */}
+
     </div>
   );
 }
--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+
 
 function Slide() {
   return (
-    <div className='slide'>
-        <div id='photo'>
-            <img src="" alt="" />
-        </div>
-        
-
+    <div className='side'>
+        test
+        <Link to="/about">About</Link>
     </div>
   )
 }
