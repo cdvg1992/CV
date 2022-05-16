0818e89c890121136e3774aebe3bd93e5554a8d9
cdvg1992 <vangroeningen.c@outlook.com>
2022-05-16 10:40:44 +0200

    add slide

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css';
 import React from 'react';
+import Slide from './components/Slide';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
 
-      hello
+      <Slide/>
    
     </div>
   );
---
+++ src/components/Slide.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Slide() {
+  return (
+    <div className='slide'>
+        <div id='photo'>
+            <img src="" alt="" />
+        </div>
+        
+
+    </div>
+  )
+}
+
+export default Slide
